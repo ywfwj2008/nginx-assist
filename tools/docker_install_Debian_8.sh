e592b8dfd57c0f6436f9203877bcf4dd2653a40f
ywfwj2008 <ywfwj2008@163.com>
2016-12-14 14:37:15 +0800

    add docker install script

---
+++ tools/docker_install_Debian_8.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# aliyun
+apt-get update && apt-get upgrade
+apt-get install -y apt-transport-https curl vim
+curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
+sed -i "s@^ExecStart=*@ExecStart=/usr/bin/dockerd --registry-mirror=https://jigxceev.mirror.aliyuncs.com@" /etc/systemd/system/multi-user.target.wants/docker.service
+systemctl daemon-reload && systemctl restart docker
