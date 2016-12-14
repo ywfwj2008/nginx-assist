1754179f4903f2f0af29265aa25cf422e2cc098f
ywfwj2008 <ywfwj2008@163.com>
2016-12-14 17:49:40 +0800

    update docker install

--- tools/docker_install_Debian_8.sh
+++ tools/docker_install_Debian_8.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # aliyun debian 8
-apt-get update && apt-get upgrade
+apt-get update
 apt-get install -y apt-transport-https curl vim
 curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
-sed -i "s@^ExecStart=*@ExecStart=/usr/bin/dockerd --registry-mirror=https://jigxceev.mirror.aliyuncs.com@" /etc/systemd/system/multi-user.target.wants/docker.service
+Ali_Mirror="--registry-mirror=https://jigxceev.mirror.aliyuncs.com"
+sed -i "s@^ExecStart=/usr/bin/dockerd.*@ExecStart=/usr/bin/dockerd $Ali_Mirror@" /etc/systemd/system/multi-user.target.wants/docker.service
 systemctl daemon-reload && systemctl restart docker
