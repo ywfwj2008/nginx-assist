e10221e3a229c644d59f6b13df5b1cbfa29b067d
ywfwj2008 <ywfwj2008@163.com>
2016-12-14 14:38:37 +0800

    update

--- tools/docker_install_Debian_8.sh
+++ tools/docker_install_Debian_8.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# aliyun
+# aliyun debian 8
 apt-get update && apt-get upgrade
 apt-get install -y apt-transport-https curl vim
 curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
