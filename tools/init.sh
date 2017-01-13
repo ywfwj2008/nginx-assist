df1f1669f062a3fcc90d859e8cb35a73b8507361
ywfwj2008 <ywfwj2008@163.com>
2017-01-13 15:31:56 +0800

    update init.sh

--- tools/init.sh
+++ tools/init.sh
@@ -2,9 +2,18 @@
 
 KODEXPLORER_VERSION=3.37
 PHPMYADMIN_VERSION=4_6_5_2
+PHPMYADMIN2_VERSION=4_4_15_9
+DEFAULT_DIR=/home/wwwroot/default
 
+# install kodexplorer
 wget --no-check-certificate -c -P /tmp http://static.kalcaddle.com/update/download/kodexplorer${KODEXPLORER_VERSION}.zip
-unzip /tmp/kodexplorer3.37.zip -d /home/wwwroot/default/kod
+unzip /tmp/kodexplorer${KODEXPLORER_VERSION}.zip -d ${DEFAULT_DIR}/kod
+unlink /tmp/kodexplorer${KODEXPLORER_VERSION}.zip
+chown -R 1000:1000 ${DEFAULT_DIR}/kod
 
+# install phpmyadmin
 wget --no-check-certificate -c -P /tmp https://github.com/phpmyadmin/phpmyadmin/archive/RELEASE_${PHPMYADMIN_VERSION}.zip
-unzip /tmp/RELEASE_4_6_5_2.zip -d /home/wwwroot/default
+unzip /tmp/RELEASE_${PHPMYADMIN_VERSION}.zip -d ${DEFAULT_DIR}
+mv ${DEFAULT_DIR}/phpmyadmin-RELEASE_${PHPMYADMIN_VERSION} ${DEFAULT_DIR}/pma
+unlink /tmp/RELEASE_${PHPMYADMIN_VERSION}.zip
+chown -R 1000:1000 ${DEFAULT_DIR}/pma
